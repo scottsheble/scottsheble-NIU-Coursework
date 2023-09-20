07e87024bfe090eb405381504dba48c1ec9894f5
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-20 00:53:11 -0500

    doc box

--- CSCI 340/Assignments-LocalClones/02b-simple-list-scottsheble-LocalClone/simple_linked_list.h
+++ CSCI 340/Assignments-LocalClones/02b-simple-list-scottsheble-LocalClone/simple_linked_list.h
@@ -1,3 +1,14 @@
+/**********************************************************************************************
+ * NIU CSCI 340 Section YOUR_SECTION                                                          *
+ * Assignment 2b                                                                               *
+ * Scott Sheble - Z1963374 - scottsheble                                                      *
+ *                                                                                            *
+ *   I certify that everything I am submitting is either provided by the professor for use in *
+ * the assignment, or work done by me personally. I understand that if I am caught submitting *
+ * the work of others (including StackOverflow or ChatGPT) as my own is an act of Academic    *
+ * Misconduct and will be punished as such.                                                   *
+ *                                                                                            *
+ **********************************************************************************************/
 #ifndef NIUCSCI340_LIST_H
 #define NIUCSCI340_LIST_H
 
